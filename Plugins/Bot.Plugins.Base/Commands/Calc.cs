98a21f337e9be0c33a318a256cf081230d90d58c
Crobol <crobol@crobol.crobol>
2012-01-30 23:12:23 +0100

    Added a math expression evaluation command (uses LoreSoft.MathExpression.dll)
    Added missing DLLs used by the IronPython-plugin

---
+++ Plugins/Bot.Plugins.Base/Commands/Calc.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using Bot.Core.Commands;
+using Meebey.SmartIrc4net;
+using LoreSoft.MathExpressions;
+
+namespace Bot.Plugins.Base.Commands
+{
+    [Export(typeof(ICommand))]
+    class Calc : Command
+    {
+        private MathEvaluator eval = new MathEvaluator();
+
+        public Calc()
+        {
+            
+        }
+
+        public override string Name
+        {
+            get { return "Calc"; }
+        }
+
+        public override string[] Aliases
+        {
+            get { return new string[] { "calc" }; }
+        }
+
+        public override string Help
+        {
+            get { return "Evaluates a mathematical expression. Parameters <expression>"; }
+        }
+
+        public override void Execute(IrcEventArgs e)
+        {
+            e.Data.Irc.SendMessage(SendType.Message, e.Data.Channel, eval.Evaluate(string.Join("", e.Data.MessageArray.Skip(1))).ToString());
+        }
+    }
+}
