7c31b55a8e042071460b206b5d1517e14c9aa224
Crobol <crobol@crobol.crobol>
2012-02-01 14:10:17 +0100

    Added command for synchronous vs asynchronous demonstration

---
+++ Plugins/Bot.Plugins.Base/Commands/SyncVsAsync.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Bot.Core.Commands;
+using Meebey.SmartIrc4net;
+
+namespace Bot.Plugins.Base.Commands
+{
+    [Export(typeof(ICommand))]
+    class SyncTest : Command
+    {
+        public SyncTest()
+        {
+
+        }
+
+        public override string Name
+        {
+            get { return "SyncTest"; }
+        }
+
+        public override string[] Aliases
+        {
+            get { return new string[] { "synctest" }; }
+        }
+
+        public override void Execute(IrcEventArgs e)
+        {
+            Thread.Sleep(3000);
+            e.Data.Irc.SendMessage(SendType.Message, e.Data.Channel, "Sync wait done");
+        }
+    }
+
+    [Export(typeof(ICommand))]
+    class AsyncTest : AsyncCommand
+    {
+        public AsyncTest()
+        {
+
+        }
+
+        public override string Name
+        {
+            get { return "SyncTest"; }
+        }
+
+        public override string[] Aliases
+        {
+            get { return new string[] { "asynctest" }; }
+        }
+
+        protected override CommandCompletedEventArgs Worker(IrcEventArgs e)
+        {
+            Thread.Sleep(3000);
+            e.Data.Irc.SendMessage(SendType.Message, e.Data.Channel, "Async wait done");
+
+            return null;
+        }
+    }
+}
