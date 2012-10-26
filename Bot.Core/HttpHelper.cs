237ebd47cd586d0a9a044dab51bab2c1d0b9fa44
Crobol <crobol@crobol.crobol>
2012-10-27 01:00:56 +0200

    Fake useragent in HTTP requests.

--- Bot.Core/HttpHelper.cs
+++ Bot.Core/HttpHelper.cs
@@ -15,6 +15,7 @@ namespace Bot.Core
             webClient.CachePolicy = new System.Net.Cache.HttpRequestCachePolicy(System.Net.Cache.HttpRequestCacheLevel.CacheIfAvailable);
             webClient.Proxy = null;
             webClient.Encoding = System.Text.Encoding.UTF8;
+            webClient.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
 
             return webClient.DownloadString(url);
         }
