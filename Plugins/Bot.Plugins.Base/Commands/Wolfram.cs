eabb3f2d3b3cfcac5c726abc9c9d2f1f66a9293e
Crobol <crobol@crobol.crobol>
2013-10-07 14:11:10 +0200

    Add Wolfram Alpha command

---
+++ Plugins/Bot.Plugins.Base/Commands/Wolfram.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using Bot.Core;
+using Bot.Core.Commands;
+using Meebey.SmartIrc4net;
+using WolframAPI;
+
+namespace Bot.Plugins.Base.Commands
+{
+    class WolframOptions
+    {
+        [DefaultOption]
+        [OptionFullName("query")]
+        public string Query { get; set; }
+
+        [OptionName("a", "all")]
+        [OptionFullName("all")]
+        public bool All { get; set; }
+    }
+
+    [Export(typeof(ICommand))]
+    [CommandAttributes("WolframAlpha", true, "wa", "wolfram", "alpha")]
+    public class Wolfram : Command
+    {
+        private const string WolframAppId = "";
+        private readonly WAClient client = new WAClient(WolframAppId);
+
+        public override IEnumerable<string> Execute(IrcEventArgs e)
+        {
+            var options = OptionParser.Parse<WolframOptions>(e.Data.Message);
+            var operation = string.Join(" ", e.Data.MessageArray.Skip(1));
+            var result = client.Solve(operation);
+            return new string[] { "Result: " + result };
+        }
+    }
+}
